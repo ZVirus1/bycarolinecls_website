89634cd66cfeeea48fcbf2d75581b01c84a63b7f
Vincent <vinzcompany12@gmail.com>
2025-11-25 12:33:55 +0700

    (fix) Fixes the compatibility issue with github pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,9 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  server: {
-    port: 3000,
+  base: '/bycarolinecls-invoice/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
   },
 })

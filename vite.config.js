b1829a671ee657ea335e04e07a0b6fbd1b69c6d3
Vincent <vinzcompany12@gmail.com>
2025-11-25 17:35:57 +0700

    (chore) Changes 'dist' output directory into 'docs' to match github pages' requirement

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,7 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   base: '/bycarolinecls-invoice/',
+  build: {
+    outDir: 'docs', // Add this line
+  },
 })
